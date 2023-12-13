51b65a95d2e3f03d1e6d4716727c3250ed429e98
root <root@ip-172-31-38-153.ec2.internal>
2023-12-13 13:44:41 +0000

    java-file

---
+++ sample.java
@@ -0,0 +1,17 @@
+public class StudentInformation{
+	public static void main(string args[])
+	{
+		print StudentInfo();
+	}
+	private static.void print StudentInfo()
+	{
+		String collegeName="AITS";
+		String Studentname="Pravallika";
+		int rollnumber="9505";
+		System.out.println("college name:"+collegename);
+		System.out.println("studentname:"+Studentname);
+		System.out.println("rollnumber:"+rollnumber);
+		System.out.println("/n welcome,"+Studentname+"!");
+		System.out.println("/n thankyou");
+	}
+}	
